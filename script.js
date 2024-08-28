6ed09bce70beeb710468d88716fdf1131684b4e8
ajp7800 <55988816+ajp7800@users.noreply.github.com>
2024-08-28 16:44:29 +0200

    display even numbers

---
+++ script.js
@@ -0,0 +1,10 @@
+let displayEven = () =>{
+    for (i=0;i<11;i++){
+        if(i%2 == 0){
+            console.log(i);
+            document.write(i);
+            document.write("<br>");
+        }
+    }
+}
+displayEven();
